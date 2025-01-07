b18942acff61c413f17bf80c591a2cde14454bc5
mstout60 <mstout@eprospector.com>
2025-01-06 22:12:43 -0600

    Implement Product details tab

--- src/server/actions/products.ts
+++ src/server/actions/products.ts
@@ -6,6 +6,7 @@ import { z } from "zod";
 import {
   createProduct as createProductDb,
   deleteProduct as deleteProductDb,
+  updateProduct as updateProductDb,
 } from "@/server/db/products";
 import { redirect } from "next/navigation";
 
@@ -22,6 +23,26 @@ export async function createProduct(
   const { id } = await createProductDb({ ...data, clerkUserId: userId });
 
   redirect(`/dashboard/products/${id}/edit?tab=countries`);
+}
+
+export async function updateProduct(
+  id: string,
+  unsafeData: z.infer<typeof productDetailsSchema>
+): Promise<{ error: boolean; message: string } | undefined> {
+  const { userId } = await auth();
+  const { success, data } = productDetailsSchema.safeParse(unsafeData);
+  const errorMessage = "There was an error updating your product";
+
+  if (!success || userId == null) {
+    return { error: true, message: errorMessage };
+  }
+
+  const isSuccess = await updateProductDb(data, { id, userId });
+
+  return {
+    error: !isSuccess,
+    message: isSuccess ? "Product details updated" : errorMessage,
+  };
 }
 
 export async function deleteProduct(id: string) {
--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -3,6 +3,7 @@ import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
 import {
   CACHE_TAGS,
   dbCache,
+  getIdTag,
   getUserTag,
   revalidateDbCache,
 } from "@/lib/cache";
@@ -14,6 +15,14 @@ export function getProducts(userId: string, { limit }: { limit?: number }) {
   });
 
   return cacheFn(userId, { limit });
+}
+
+export function getProduct({ id, userId }: { id: string; userId: string }) {
+  const cacheFn = dbCache(getProductInternal, {
+    tags: [getIdTag(id, CACHE_TAGS.products)],
+  });
+
+  return cacheFn({ id, userId });
 }
 
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
@@ -45,6 +54,26 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
   return newProduct;
 }
 
+export async function updateProduct(
+  data: Partial<typeof ProductTable.$inferInsert>,
+  { id, userId }: { id: string; userId: string }
+) {
+  const { rowCount } = await db
+    .update(ProductTable)
+    .set(data)
+    .where(and(eq(ProductTable.clerkUserId, userId), eq(ProductTable.id, id)));
+
+  if (rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    });
+  }
+
+  return rowCount > 0;
+}
+
 export async function deleteProduct({
   id,
   userId,
@@ -72,5 +101,12 @@ function getProductsInternal(userId: string, { limit }: { limit?: number }) {
     where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
     orderBy: ({ createdAt }, { desc }) => desc(createdAt),
     limit,
+  });
+}
+
+function getProductInternal({ id, userId }: { id: string; userId: string }) {
+  return db.query.ProductTable.findFirst({
+    where: ({ clerkUserId, id: idCol }, { eq, and }) =>
+      and(eq(clerkUserId, userId), eq(idCol, id)),
   });
 }
